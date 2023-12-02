c0e230ad1871c9dc584d120cf39178b4650f3d0f
Pablo George <pablogeokar@hotmail.com>
2023-12-02 14:29:48 -0300

    listProps

--- src/components/ChatListItem/index.tsx
+++ src/components/ChatListItem/index.tsx
@@ -1,27 +1,39 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
-// import { Container } from './styles';
+type ChatProps = {
+  chat: {
+    id: string;
+    user: {
+      image: string;
+      name: string;
+    };
+    lastMessage: {
+      text: string;
+      createdAt: string;
+    };
+  };
+};
 
-const ChatListItem: React.FC = () => {
+const ChatListItem: React.FC<ChatProps> = ({ chat }: ChatProps) => {
   return (
     <View style={styles.container}>
       <Image
         source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+          uri: chat.user.image,
         }}
         style={styles.image}
       />
       <View style={styles.content}>
         <View style={styles.row}>
           <Text numberOfLines={1} style={styles.name}>
-            Lukas
+            {chat.user.name}
           </Text>
-          <Text style={styles.subTitle}>8:30</Text>
+          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
         </View>
 
         <Text numberOfLines={2} style={styles.subTitle}>
-          Hello There
+          {chat.lastMessage.text}
         </Text>
       </View>
     </View>
